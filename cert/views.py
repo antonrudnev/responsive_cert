30d8fb800692c2b3c81077dad0d23093ee5a3750
Anton Rudnev <anton.rudnev@gmail.com>
2018-12-12 13:47:16 -0500

    Authentication through login form is implemented.

--- cert/views.py
+++ cert/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 
 from .models import Profile
 
 
-class UserListView(ListView):
+class UserListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Profile.objects.all()
--- microcredentials/urls.py
+++ microcredentials/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cert/', include('cert.urls')),
+    path('login/', auth_views.LoginView.as_view()),
+    path('logout/', auth_views.LogoutView.as_view()),
 ]
